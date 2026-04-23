856b239a28c8a3b7b045a21bd07b226d51f687c0
Santiago Arevalo <santif.arevalo@gmail.com>
2026-04-22 22:39:24 -0500

    first commit

---
+++ src/main/java/u/sysnotification/SysNotificationApplication.java
@@ -0,0 +1,13 @@
+package u.sysnotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SysNotificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/u/sysnotification/SysNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package u.sysnotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysNotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
